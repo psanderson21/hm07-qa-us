d4e3dcdc0c215eb8929fa229ea501cf66a585b91
Patrick Sanderson <patrick.sanderson21@gmail.com>
2024-03-05 23:33:54 -0600

    second commit updated URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-0f773093-25f6-4892-890b-66a06496d72b.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-ba69df95-33ed-42c5-a540-400ad103e475.containerhub.tripleten-services.com"
 }
